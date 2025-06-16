a80f5be423f570a017f44953ab0df62eb599c476
shazywd <shazyp6@gmail.com>
2025-06-16 09:24:38 +0500

    remove extra code

--- src/features/language/languageSlice.tsx
+++ src/features/language/languageSlice.tsx
@@ -9,16 +9,6 @@ const languageSlice = createSlice({
   name: "language",
   initialState,
   reducers: {
-    toggleLanguage: (state) => {
-      const newLang = state.currentLanguage === "en" ? "ar" : "en";
-      state.currentLanguage = newLang;
-
-      i18n.changeLanguage(newLang);
-      localStorage.setItem("i18nextLng", newLang); // ✅ manual storage
-
-      document.documentElement.lang = newLang;
-      document.documentElement.dir = newLang === "ar" ? "rtl" : "ltr";
-    },
     setLanguage: (state, action) => {
       const lang = action.payload === "ar" ? "ar" : "en";
       state.currentLanguage = lang;
@@ -29,5 +19,5 @@ const languageSlice = createSlice({
   },
 });
 
-export const { toggleLanguage, setLanguage } = languageSlice.actions;
+export const { setLanguage } = languageSlice.actions;
 export default languageSlice.reducer;
